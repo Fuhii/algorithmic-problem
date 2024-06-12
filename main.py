386b87c797ca32f276df694e4211d86c7f40a900
伊藤史人 <ittak@MacBook-Pro-2.local>
2024-06-13 02:22:46 +0900

    ロジックの説明と問題の説明を追加

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-currentBoard = [
-[ 0, 0, 0, 0, 0, 0, 0, 0],
-[ 0, 0, 2, 2, 1, 1, 1, 0],
-[ 0, 0, 1, 2, 1, 1, 0, 0],
-[ 0, 0, 0, 2, 1, 1, 0, 0],
-[ 0, 0, 0, 2, 1, 0, 0, 0],
-[ 0, 0, 0, 0, 0, 0, 0, 0],
-[ 0, 0, 0, 0, 0, 0, 0, 0],
-[ 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
 def checkAndMarkRow(board):
+  # ある配列の要素が２である時それよりあとの要素に１が続いておりその後０が現れた場所を３に置き換える処理
   for j in range(len(board)):
       if board[j] == 2:
         currentIndex = j
@@ -17,7 +7,7 @@ def checkAndMarkRow(board):
             currentIndex += 1
         if currentIndex != len(board)-1 and currentIndex != j:
           board[currentIndex+1] = 3
-
+  # 配列を逆順にして同様にして３に置き換える処理
   for h in range(len(board)):
     if board[h] == 2:
       currentIndexReverse = h
@@ -34,6 +24,7 @@ def markRows(board):
   return board
 
 def markColumns(board):
+  # 二重配列のたての要素を取得し３に置き換える処理
   newBoard = []
   for h in range(8):
     for i in range(8):
@@ -49,17 +40,19 @@ def markColumns(board):
 
 def markDiagonals(board):
   numBoard = len(board)
-  # 右下→左上
+  # 二重配列の右下→左上の斜めの要素を取得し３に置き換える処理
+  # iは対角線の数を表しhはその対角線に含まれる要素の数を表す。
   for i in range(1-numBoard, numBoard):
     diagonal = []
     for h in range(numBoard-abs(i)):
       diagonal.append(board[max(0, -i)+h][max(0, i)+h])
     markedDiagonal = checkAndMarkRow(diagonal)
+    # ３をおける場所のindexを特定して置き換える処理
     for j in range(len(markedDiagonal)):
       if markedDiagonal[j] == 3:
         board[max(0, -i)+j][max(0, i)+j] = 3
 
-  # 左下→右上
+  # 二重配列の左下→右上の斜めの要素を取得し３に置き換える処理
   for i in range(1-numBoard, numBoard):
     diagonal = []
     for h in range(numBoard-abs(i)):
@@ -72,9 +65,37 @@ def markDiagonals(board):
 
 
 def canPutTwoBoard(board):
+  # 横方向を探索し３に置き換える処理
   markRows(board)
+  # 縦方向を探索し３に置き換える処理
   markColumns(board)
+  # 斜め方向を探索し３に置き換える処理
   markDiagonals(board)
   return board
 
-print(canPutTwoBoard(currentBoard))
+if __name__ == "__main__":
+  currentBoard = [
+    [ 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 2, 2, 1, 1, 1, 0],
+    [ 0, 0, 1, 2, 1, 1, 0, 0],
+    [ 0, 0, 0, 2, 1, 1, 0, 0],
+    [ 0, 0, 0, 2, 1, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+  print(canPutTwoBoard(currentBoard))
+
+  #　問題
+  #　以下のような二周配列が与えられたとする。それをオセロの盤面と見た時に０は空白、１は白、２は黒と考える。
+  #　その時２をおくことのできる０の場所を３に置き換える処理を実装してください。
+  # currentBoard = [
+  #   [ 0, 0, 0, 0, 0, 0, 0, 0],
+  #   [ 0, 0, 2, 2, 1, 1, 1, 0],
+  #   [ 0, 0, 1, 2, 1, 1, 0, 0],
+  #   [ 0, 0, 0, 2, 1, 1, 0, 0],
+  #   [ 0, 0, 0, 2, 1, 0, 0, 0],
+  #   [ 0, 0, 0, 0, 0, 0, 0, 0],
+  #   [ 0, 0, 0, 0, 0, 0, 0, 0],
+  #   [ 0, 0, 0, 0, 0, 0, 0, 0]
+  # ];
